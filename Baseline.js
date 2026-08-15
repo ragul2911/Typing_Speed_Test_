696358fce138ded2e57ae6e5cc529d771476786f
Ragul <kragul2911@gmail.com>
2026-08-15 21:46:45 +0530

    Add files via upload

---
+++ Baseline.js
@@ -0,0 +1,12 @@
+let wpm = localStorage.getItem("wpm");
+let accuracy = localStorage.getItem("accuracy");
+let char = localStorage.getItem("Characters");
+const retype = document.getElementById("restrt");
+
+retype.addEventListener("click",()=>{
+    window.location.href = "index.html";
+})
+document.getElementById("wpm4").innerText = wpm;
+document.getElementById("wpm5").innerText = wpm;
+document.getElementById("output").innerText = accuracy + "%";
+document.getElementById("output1").innerText = char;
