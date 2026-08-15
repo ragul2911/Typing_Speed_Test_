cd2200cd0e77a3e6c6d16e20795e651d8acc9091
Ragul <kragul2911@gmail.com>
2026-08-15 21:29:58 +0530

    Add files via upload

---
+++ mobile/testComplete.js
@@ -0,0 +1,15 @@
+let wpm = localStorage.getItem("wpm");
+let accuracy = localStorage.getItem("accuracy");
+let char = localStorage.getItem("Characters");
+const retype = document.getElementById("restrt");
+// console.log(wpm);
+// console.log(accuracy);
+// console.log(char);
+document.getElementById("wpm4").innerText = wpm;
+document.getElementById("wpm5").innerText = wpm;
+document.getElementById("output").innerText = accuracy + "%";
+document.getElementById("output1").innerText = char;
+
+retype.addEventListener("click",()=>{
+    window.location.href = "index.html";
+})
